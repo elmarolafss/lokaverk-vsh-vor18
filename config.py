f8a95cd702df953c6353b54ed0f3604066b614d4
Örn Óli Strange <ornst278@nemi.tskoli.is>
2018-04-15 20:10:10 +0000

    sma sql og database uppsetning

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'leyni-lykill'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ late/models.py
@@ -0,0 +1,60 @@
+from late import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(128), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+	joined = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+	def __repr__(self):
+		return f"<User: {self.username}>"
+
+class Product(db.Model):
+	link_name = db.Column(db.string(255), primary_key=True)
+	name = db.Column(db.string(255), index=True, unique=True)
+	price = db.Column(db.Float, index=True)
+	discount = db.Column(db.Float)
+	category = db.Column(db.string(255), index=True)
+	color = db.Column(db.string(255), index=True)
+	colors = db.Column(db.string(255), index=True)
+	images = db.Column(db.string(255), index=True)
+	info = db.Column(db.string(255), index=True)
+	sizes = db.Column(db.string(255))
+
+	def colors_object(self):
+		return Color(self.colors)
+
+	def make_arry(self, str_arr, delimiter):
+		return str_arr.split(delimiter)
+
+	def __repr__(self):
+		return self.name
+
+	def __str__(self):
+		return self.link_name
+
+class Purchase(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	prod_name = db.Column(db.string(255), db.ForeignKey('product.link_name'))
+	cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
+	price = db.Column(db.Float, index=True)
+	discount = db.Column(db.Float, index=True)
+	color = db.Column(db.string(255), index=True)
+	size = db.Column(db.string(255), index=True)
+	date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+	def __repr__(self):
+		return f"item: {this.prod_name} added to cart: {cart_id}"
+
+class Cart(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	total_pre_tax = db.Column(db.Float, index=True)
+	discount = db.Column(db.Float, index=True)
+	tax = db.Column(db.Float, index=True)
+	total = db.Column(db.Float, index=True)
+	color = db.Column(db.string(255), index=True)
+
+	def __repr__(self):
+		return self.id
